4edb7395d720de96ecdcfe0febfe795127840745
Zarko <zarkoned@outloo.com>
2025-08-19 19:33:30 +0200

    primer 8

---
+++ JavaSwing-Cas2/src/primer8/BorderLayoutExample.java
@@ -0,0 +1,25 @@
+package primer8;
+import javax.swing.*;
+import java.awt.*;
+
+public class BorderLayoutExample {
+    public static void main(String[] args) {
+        // Kreiranje prozora (JFrame)
+        JFrame frame = new JFrame("BorderLayout Primer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+
+        // Postavljanje BorderLayout (podrazumevano za JFrame)
+        frame.setLayout(new BorderLayout(10, 10)); // horizontalni i vertikalni razmak
+
+        // Dodavanje komponenti u razlicite regione
+        frame.add(new JButton("Sever (NORTH)"), BorderLayout.NORTH);
+        frame.add(new JButton("Jug (SOUTH)"), BorderLayout.SOUTH);
+        frame.add(new JButton("Istok (EAST)"), BorderLayout.EAST);
+        frame.add(new JButton("Zapad (WEST)"), BorderLayout.WEST);
+        frame.add(new JButton("Centar (CENTER)"), BorderLayout.CENTER);
+
+        // Prikaz prozora
+        frame.setVisible(true);
+    }
+}
