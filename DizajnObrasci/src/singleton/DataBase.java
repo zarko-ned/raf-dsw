f348f8e0255c6a3600bc8e9c651d381d0c5cc601
Zarko <zarkoned@outloo.com>
2025-08-27 17:14:12 +0200

    singleton1

---
+++ DizajnObrasci/src/singleton/DataBase.java
@@ -0,0 +1,51 @@
+package singleton;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataBase {
+    // Statička promenljiva koja čuva jedinstvenu instancu
+    private static DataBase instance;
+
+    // Mapa koja simulira bazu podataka
+    private Map<Integer, Object> databaseMap;
+
+    // Privatni konstruktor sprečava instanciranje spolja
+    private DataBase() {
+        databaseMap = new HashMap<>();
+        System.out.println("DataBase instance created.");
+    }
+
+    // Statička metoda koja vraća jedinstvenu instancu
+    public static synchronized DataBase getInstance() {
+        if (instance == null) {
+            instance = new DataBase();
+        }
+        return instance;
+    }
+
+    // Metoda za insertovanje objekta u mapu
+    public void insert(Integer key, Object value) {
+        databaseMap.put(key, value);
+        System.out.println("Inserted object with key: " + key);
+    }
+
+    // Metoda za dobijanje objekta iz mape
+    public Object get(Integer key) {
+        return databaseMap.get(key);
+    }
+
+    // Metoda za prikaz svih objekata u mapi
+    public void displayAll() {
+        System.out.println("Database contents:");
+        for (Map.Entry<Integer, Object> entry : databaseMap.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+    }
+
+    // Metoda za brisanje objekta iz mape
+    public void delete(Integer key) {
+        databaseMap.remove(key);
+        System.out.println("Deleted object with key: " + key);
+    }
+}
