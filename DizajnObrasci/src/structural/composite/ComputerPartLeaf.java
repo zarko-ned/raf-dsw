635c6adc4009ded9b6b8a431c2d1babfdde761ee
Zarko <zarkoned@outloo.com>
2025-08-31 11:24:14 +0200

    composite l

--- DizajnObrasci/src/structural/composite/ComputerPartLeaf.java
+++ DizajnObrasci/src/structural/composite/ComputerPartLeaf.java
@@ -1,7 +1,7 @@
 package structural.composite;
 
 // Computer part composite predstavlja node leaf
-public abstract class ComputerPartLeaf extends ComputerPart{
+public abstract class ComputerPartLeaf extends ComputerPart {
     public ComputerPartLeaf(String name, double price) {
         super(name, price);
     }
